5d972285319346318f250d6b384626c4028c044b
Elitsa Stancheva <99178411+Eli-Stancheva@users.noreply.github.com>
2022-08-03 15:38:41 +0300

    Add files via upload

---
+++ LAB_4_InterfacesAndAbstraction/P6_Ferrari/Car.java
@@ -0,0 +1,6 @@
+package OOP.LAB_4_InterfacesAndAbstraction.P6_Ferrari;
+
+public interface Car {
+    String brakes();
+    String gas();
+}
---
+++ LAB_4_InterfacesAndAbstraction/P6_Ferrari/Main.java
@@ -0,0 +1,7 @@
+package OOP.LAB_4_InterfacesAndAbstraction.P6_Ferrari;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
