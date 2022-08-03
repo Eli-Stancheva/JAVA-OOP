0277fa0447630e2c85c8cbae05b0d441fdaab938
Elitsa Stancheva <99178411+Eli-Stancheva@users.noreply.github.com>
2022-08-03 15:33:37 +0300

    Add files via upload

---
+++ LAB_3_Inheritance/P4_RandomArrayList/Main.java
@@ -0,0 +1,7 @@
+package OOP.LAB_3_Inheritance.P4_RandomArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ LAB_3_Inheritance/P4_RandomArrayList/RandomArrayList.java
@@ -0,0 +1,15 @@
+package OOP.LAB_3_Inheritance.P4_RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList {
+    private Random random;
+
+    public Object getRandomElement(){
+        int index = this.random.nextInt(super.size());
+        Object element = super.get(index);
+        super.remove(index);
+        return element;
+    }
+}
