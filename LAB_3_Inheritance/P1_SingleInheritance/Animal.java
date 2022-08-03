b3b2ec5a7821c79b6b3b1ac8235c6ca0ec66a061
Elitsa Stancheva <99178411+Eli-Stancheva@users.noreply.github.com>
2022-08-03 15:31:12 +0300

    Add files via upload

---
+++ LAB_3_Inheritance/P1_SingleInheritance/Animal.java
@@ -0,0 +1,7 @@
+package OOP.LAB_3_Inheritance.P1_SingleInheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ LAB_3_Inheritance/P1_SingleInheritance/Dog.java
@@ -0,0 +1,7 @@
+package OOP.LAB_3_Inheritance.P1_SingleInheritance;
+
+public class Dog extends Animal{
+    public void bark(){
+        System.out.println("barking...");
+    }
+}
