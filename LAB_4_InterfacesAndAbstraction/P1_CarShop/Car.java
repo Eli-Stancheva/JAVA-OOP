41bb751bb9c4e8a17a67e2021e2ffd08e356ee92
Elitsa Stancheva <99178411+Eli-Stancheva@users.noreply.github.com>
2022-08-03 15:35:15 +0300

    Add files via upload

---
+++ LAB_4_InterfacesAndAbstraction/P1_CarShop/Car.java
@@ -0,0 +1,15 @@
+package OOP.LAB_4_InterfacesAndAbstraction.P1_CarShop;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    Integer TIRES = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+}
