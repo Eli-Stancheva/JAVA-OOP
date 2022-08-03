4594cb3f126a57cfdc74475b3862af199fed60f3
Elitsa Stancheva <99178411+Eli-Stancheva@users.noreply.github.com>
2022-08-03 10:21:57 +0300

    Add files via upload

---
+++ EXERCISE_5_Polymorphism/P3_WildFarm/Meat.java
@@ -0,0 +1,7 @@
+package OOP.EXERCISE_5_Polymorphism.P3_WildFarm;
+
+public class Meat extends Food{
+    public Meat(Integer quantity) {
+        super(quantity);
+    }
+}
---
+++ EXERCISE_5_Polymorphism/P3_WildFarm/Vegetable.java
@@ -0,0 +1,8 @@
+package OOP.EXERCISE_5_Polymorphism.P3_WildFarm;
+
+public class Vegetable extends Food{
+
+    public Vegetable(Integer quantity) {
+        super(quantity);
+    }
+}
